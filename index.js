ab2c6da2fc284c7a9bbf2e92f9e781f2d40d4a68
Tux <write@imaginarycode.com>
2015-10-17 12:28:39 -0400

    Fix for challenge

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var crypto = require('crypto');
 var net = require('net');
 
-function createMessage(header, vote, sig) {
+function createMessage(header, vote) {
     var data = header.split(' ');
-    var challenge = data[2].substring(0, data[2].length - 1);
-    var message = JSON.stringify({challenge: challenge, payload: vote, signature: sig});
+
+    if (data.length != 3) {
+        throw new Error("Not a Votifier v2 protocol server");
+    }
+
+    vote['challenge'] = data[2].substring(0, data[2].length - 1);
+    var voteAsJson = JSON.stringify(vote);
+    var digest = crypto.createHmac('sha256', options.token);
+    digest.update(voteAsJson);
+    var sig = digest.digest('base64');
+
+    var message = JSON.stringify({payload: JSON.stringify(vote), signature: sig});
     var messageBuffer = new Buffer(message.length + 4);
     messageBuffer.writeUInt16BE(0x733a);
     messageBuffer.writeUInt16BE(message.length, 2);
@@ -21,18 +31,13 @@ module.exports = exports = function vote(options, cb) {
         return cb(new Error("missing username, address, timestamp, or serviceName in 'vote'"));
     }
 
-    var voteAsJson = JSON.stringify(vote);
-    var digest = crypto.createHmac('sha256', options.token);
-    digest.update(voteAsJson);
-    var sig = digest.digest('base64');
-
     var socket = net.createConnection(options.port, options.host);
     socket.once('data', function (buf) {
         var returnError = function () {
             cb(new Error('Unexpected error'));
         };
         socket.once('end', returnError);
-        socket.write(createMessage(buf.toString(), voteAsJson, sig));
+        socket.write(createMessage(buf.toString(), vote));
         socket.once('data', function (respBuf) {
             var resp = JSON.parse(respBuf.toString());
             socket.removeListener('end', returnError);
